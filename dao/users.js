05981ec24a24dd56a38a2fc9ec2ebf61c2fd2760
erickv <erickv@bluetrailsoft.com>
2022-09-13 16:46:33 -0500

    create user database access obj

---
+++ dao/users.js
@@ -0,0 +1,39 @@
+"use strict";
+const query = require("../database/query");
+
+/**
+ * @param { String } username
+ * @param { String } password
+ * @returns { Object }
+ */
+const createUser = (username, password) => {
+  const userType = 1; // common
+  const sqlQuery =
+    `INSERT INTO users (
+        uuid,
+        user_role_id,
+        username,
+        password,
+        created_at,
+        updated_at
+    ) SELECT
+        gen_random_uuid(),
+        $1,
+        $2::VARCHAR(255),
+        $3::VARCHAR(255),
+        NOW(), NOW()
+      WHERE NOT EXISTS (
+        SELECT *
+        FROM users WHERE
+        username = $2)
+      RETURNING uuid;`
+    const replacements = [
+      userType,
+      username,
+      password
+    ];
+
+    return query(sqlQuery, replacements);
+};
+
+module.exports = { createUser };
