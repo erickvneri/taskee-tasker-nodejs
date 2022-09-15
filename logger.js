f787416ce8004e328a7fc145ab633d7adf6d78cc
erickv <erickv@bluetrailsoft.com>
2022-09-15 18:29:28 -0500

    JSON supp for simple logger

--- logger.js
+++ logger.js
@@ -2,11 +2,26 @@
 
 const prefix = (level) => `${new Date().toISOString()} - ${level} - `;
 
+/**
+ * @param { * } item
+ * @param { Number } index
+ * @param { Array } arr
+ * @returns { * }
+ */
+const mapIfJSON = (item, index, arr) => typeof item === "object" ? JSON.stringify(item) : item
+
+/**
+ * global console wrapper
+ * to enhance information
+ * across loggins.
+ */
 module.exports = {
   info: (...args) => console.log(
-    prefix("INFO") + args.join("    ")
+    prefix("INFO") +
+    args.map(mapIfJSON).join(" - ")
   ),
   warn: (...args) => console.log(
-    prefix("WARN") + args.join("    ")
+    prefix("WARN") +
+    args.map(mapIfJSON).join(" - ")
   )
 };
